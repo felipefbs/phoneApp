65db32bd1385dc594bf6b4ae945c07a7d0d5c78f
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-07-19 14:00:04 -0300

    chore: added some comments to main file

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ const UserController = require("./controllers/userController");
 // Importa a conexão com o banco de dados
 const database = require("./infra/sqlite-db");
 
+// Importa objeto de acesso aos dados
 const UserDAO = require("./dao/usersDAO");
 
-const userDAO = new UserDAO(database);
+// Instancia um novo DAO para ser utilizado pelos controllers
+const usersDAO = new UserDAO(database);
 
 // Instancia os Controllers das entidades
 const foneController = new FoneController(database);
-const userController = new UserController(userDAO);
+const userController = new UserController(usersDAO);
 
 // Cria uma nova aplicação express
 const app = express();
@@ -24,19 +26,25 @@ const app = express();
 // Utiliza o middleware para dar json parse em todas as requisições
 app.use(express.json());
 
+// Definição de uma rota de teste
 app.get("/", function (req, res) {
   res.send({ message: "Hello, World!" });
 });
 
+// Definição do conjunto de rotas dos fones
 app.get("/fones/:model", foneController.show);
 app.get("/fones", foneController.index);
 app.post("/fones", foneController.store);
 app.delete("/fones/:model", foneController.delete);
 
+// Definição do conjunto de rotas dos usuários
 app.get("/users/:name", userController.show);
 app.get("/users", userController.index);
 app.post("/users", userController.store);
 app.put("/users/:name", userController.update);
 app.delete("/users/:name", userController.delete);
 
-app.listen(APP_PORT);
+// Servidor escutando na porta APP_PORT=3000
+app.listen(APP_PORT, () => {
+  console.log(`listening on port ${APP_PORT}`);
+});
