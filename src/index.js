b86b169c3dee8624cfd09b0e1f3c9e75379646a1
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-07-19 11:04:27 -0300

    chore: added some

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 
+// Define a porta utilizada pelo servidor
 const { APP_PORT } = require("./utils/appConfig");
 
+// Importa as classes que controlam as requisições dos Fones e do Usuários
 const FoneController = require("./controllers/foneController");
 const UserController = require("./controllers/userController");
 
+// Importa a conexão com o banco de dados
 const database = require("./infra/sqlite-db");
 
+const UserDAO = require("./dao/usersDAO");
+
+const userDAO = new UserDAO(database);
+
+// Instancia os Controllers das entidades
+const foneController = new FoneController(database);
+const userController = new UserController(userDAO);
+
+// Cria uma nova aplicação express
 const app = express();
 
-const foneController = new FoneController(database);
-const userController = new UserController(database);
-
-app.use(function (req, res, next) {
-  console.log("Time:", Date.now());
-  next();
-});
-
+// Utiliza o middleware para dar json parse em todas as requisições
 app.use(express.json());
 
 app.get("/", function (req, res) {
