bfedfb03ed83d3ff560d847b1654ccc83d04f785
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-07-22 09:12:01 -0300

    feat: add cors allow all to API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 
 // Define a porta utilizada pelo servidor
 const { APP_PORT } = require("./utils/appConfig");
@@ -23,8 +24,23 @@ const userController = new UserController(usersDAO);
 // Cria uma nova aplicação express
 const app = express();
 
+// Utiliza o middleware do cors para permitir todas as requisições
+app.use(cors());
+
 // Utiliza o middleware para dar json parse em todas as requisições
 app.use(express.json());
+
+// Middleware criado para fazer o log de todas as requisições feitas para a API
+app.use((req, res, next) => {
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  let log = `${method}:${url} ${status} ${ip}`;
+  console.log(log);
+  next();
+});
 
 // Definição de uma rota de teste
 app.get("/", function (req, res) {
